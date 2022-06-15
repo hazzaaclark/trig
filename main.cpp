caa1f38e1cbe6c9ec142cc52f337ad5e8b59a395
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-06-15 13:37:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
     float z[1760];
     char b[1760];
     printf("\x1b[2J");
-    for(;;) {
+    for(;;) 
+    {
         memset(b,32,1760);
         memset(z,0,7040);
         for(j=0; j < 6.28; j += 0.07) {
